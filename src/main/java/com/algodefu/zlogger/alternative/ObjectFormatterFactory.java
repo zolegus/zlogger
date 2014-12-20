03c9380d7a5106debb93fef8bd5183bf6122f55b
Oleg Zherebkin <zolegus@gmail.com>
2014-12-21 00:14:24 +0300

    пережитки будущего видимо появятся только в версии 2.0

--- src/main/java/com/algodefu/zlogger/alternative/ObjectFormatterFactory.java
+++
@@ -1,9 +0,0 @@
-package com.algodefu.zlogger.alternative;
-
-/**
- * @author oleg.zherebkin
- */
-public interface ObjectFormatterFactory {
-
-	ObjectFormatter getObjectFormatter(final Object obj);
-}
