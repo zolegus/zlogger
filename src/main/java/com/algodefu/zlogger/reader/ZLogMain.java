8e77b4987960328a706269cc5e54e12944a0273e
Oleg Zherebkin <zolegus@gmail.com>
2015-01-31 15:05:42 +0300

    добавлен ключ -onlycount и отформатировано сообщение помощи

--- src/main/java/com/algodefu/zlogger/reader/ZLogMain.java
+++ src/main/java/com/algodefu/zlogger/reader/ZLogMain.java
@@ -4,7 +4,9 @@ import com.algodefu.zlogger.ZLogLevel;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author oleg.zherebkin
@@ -19,21 +21,22 @@ public class ZLogMain {
         if (args[0].equals("--help") || args[0].equals("--usage") ) {
             //TODO Нужно перенести и выводить из ресурсов
             System.out.println("Синтаксис: zlog ПУТЬ {ПАРАМЕТРЫ}\n" +
-                    "ПУТЬ - определяет расположение хранилище z-логов\n" +
-                    "ПАРАМЕТРЫ: Условия фильтрациии лог-сообщений" +
-                    "-b Время первого сообщения, в формате YYYYMMDD-HHMMSS.\n" +
-                    "-e Время последнего сообщения, в формате YYYYMMDD-HHMMSS.\n" +
-                    "-l  Уровень логирования. Варианты: TRACE, DEBUG, INFO, WARN, ERROR. По умолчанию TRACE.\n" +
-                    "-t Название потока. Обязательно полное имя. Не фильтрует по части названия.\n" +
-                    "-c Название класса. Обязательно полное имя без пакетов. Не фильтрует по части названия.\n" +
-                    "-m Сообщение. Можно указывать часть сообщения или даже один символ.\n" +
-                    "-n Количество записей, которые будут выданы. По умолчанию 50. Если указать 0, то полное содержание.");
+                    "ПУТЬ\t\tОбязательный параметр, определяет расположение хранилище z-логов\n" +
+                    "ПАРАМЕТРЫ\tУсловия фильтрациии лог-сообщений:\n\n" +
+                    " -b\t\t\tВремя первого сообщения, в формате yyyyMMdd-HH:mm:ss.SSS\n" +
+                    " -e\t\t\tВремя последнего сообщения, в формате yyyyMMdd-HH:mm:ss.SSS\n" +
+                    " -l\t\t\tУровень логирования. Варианты: TRACE, DEBUG, INFO, WARN, ERROR. По умолчанию TRACE\n" +
+                    " -t\t\t\tНазвание потока. Обязательно полное имя. Не фильтрует по части названия\n" +
+                    " -c\t\t\tНазвание класса. Обязательно полное имя без пакетов. Не фильтрует по части названия\n" +
+                    " -m\t\t\tСообщение. Можно указывать часть сообщения или даже один символ\n" +
+                    " -n\t\t\tКоличество записей, которые будут выданы. По умолчанию 50. Для получения всех записей указать 0\n" +
+                    " -onlycount\tВывести только количество найденных записей. Блокирует ключ -n");
             return;
         }
 
         if (args[0].equals("--version")) {
             //TODO Нужно перенести и выводить из ресурсов
-            System.out.println("ZLogger 1.1-SNAPSHOT version");
+            System.out.println("zlogger 1.1-SNAPSHOT");
             return;
         }
 
@@ -45,12 +48,16 @@ public class ZLogMain {
             return;
         }
 
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd-HH:mm:ss.SSS");
+        long beginTimestamp = 0;
+        long endTimestamp = Long.MAX_VALUE;
         String parameter = "";
         ZLogLevel logLevel = ZLogLevel.TRACE;
         String threadName = "";
         String className = "";
         String message = "";
         int numMessages = 0;
+        boolean onlyCount = false;
 
         try {
             if (args.length > 1) {
@@ -58,10 +65,14 @@ public class ZLogMain {
                     parameter = args[i];
                     if (parameter.equals("-b")) {
                         // Парсим время начала
+                        LocalDateTime dateTime = LocalDateTime.parse(args[++i], dtf);
+                        beginTimestamp = dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
                     }
 
                     if (parameter.equals("-e")) {
                         // Парсим время конца
+                        LocalDateTime dateTime = LocalDateTime.parse(args[++i], dtf);
+                        endTimestamp = dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
                     }
 
                     if (parameter.equals("-l")) {
@@ -89,6 +100,11 @@ public class ZLogMain {
                         numMessages = Integer.parseInt(args[++i]);
                         continue;
                     }
+                    if (parameter.equals("-onlycount")) {
+                        // Количество записей
+                        onlyCount = true;
+                        continue;
+                    }
                 }
             }
         } catch (Exception ex) {
@@ -99,17 +115,22 @@ public class ZLogMain {
 
         try {
             ZLogReader reader = new ZLogReader(BASE_PATH);
-            String[] result = reader.search(0, Long.MAX_VALUE, logLevel, threadName, className, message, 1);
+            String[] result = reader.search(beginTimestamp, endTimestamp, logLevel, threadName, className, message, 1);
             int n = 1;
             if (result.length == 0)
                 System.out.println("Ничего не найдено!");
             else {
                 while (result.length > 0) {
-                    System.out.println(result[0]);
-                    if (numMessages != 0 && n++ >= numMessages)
+                    if (onlyCount)
+                        n++;
+                    else
+                        System.out.println(result[0]);
+                    if (!onlyCount && numMessages != 0 && n++ >= numMessages)
                         break;
                     result = reader.next();
                 }
+                if (onlyCount)
+                    System.out.printf("Найдено %d записей%n", n-1);
             }
         } catch (IOException ex) {
             System.out.printf("Проблема с чтением данных из хранилища. %s.%n", ex.getMessage());
